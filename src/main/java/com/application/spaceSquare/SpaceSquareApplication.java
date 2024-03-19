7a4803d70f95a1d506e101cc17a5cc530ddcc3af
xd1124 <sjtand14@gmail.com>
2024-03-19 17:51:52 +0900

    gitignore파일

---
+++ src/main/java/com/application/spaceSquare/SpaceSquareApplication.java
@@ -0,0 +1,13 @@
+package com.application.spaceSquare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpaceSquareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpaceSquareApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/application/spaceSquare/SpaceSquareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.application.spaceSquare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpaceSquareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
